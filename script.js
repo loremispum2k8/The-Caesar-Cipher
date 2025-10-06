3fa13998c08c1020fb71b93c420aab55c0b1ec52
Loremispum <141552551+loremispum2k8@users.noreply.github.com>
2025-10-06 21:59:24 +0300

    Add decodification

--- script.js
+++ script.js
@@ -5,12 +5,23 @@ let shiftValue = document.querySelector('#shift')
 let codedMessageInput = document.querySelector('#codeMessage')
 let codeBtn = document.querySelector('#codeBtn')
 let deleteBtn = document.querySelector('#deleteBtn')
+let decode = document.querySelector('#decode')
 
 codeBtn.addEventListener('click',()=>{
+    shiftValue.value =Math.floor(Math.abs(shiftValue.value))
     codedMessageInput.value = caesarCipher(messageInput.value,Number(shiftValue.value))
     messageInput.value = ''
 })
 deleteBtn.addEventListener("click",()=>{
     messageInput.value = ''
+    codedMessageInput.value = ''
+})
+decode.addEventListener('click',()=>{
+    if(shiftValue.value < 0){
+        shiftValue.value = Math.ceil(shiftValue.value)
+    }else{
+        shiftValue.value = -Math.floor(shiftValue.value)
+    }
+    messageInput.value= caesarCipher(codedMessageInput.value,Number(shiftValue.value))
     codedMessageInput.value = ''
 })
