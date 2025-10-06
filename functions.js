ef0cbe482e9ccb35054a9cfd985423ffe565f311
Loremispum <141552551+loremispum2k8@users.noreply.github.com>
2025-10-06 21:48:42 +0300

    Add codifying test sight

--- functions.js
+++ functions.js
@@ -1,4 +1,5 @@
 export function caesarCipher(string,shift){
+        shift = shift % 26
         let encMsg = '';
         let alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
--- script.js
+++ script.js
@@ -1,37 +1,16 @@
-function caesarCipher(string,shift){
-        let encMsg = '';
-        let alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+import {caesarCipher} from "./functions.js"
 
-        arr = string.split('');
-        for(letter of arr){
-                let letterFound = false
-                let smallerOrBigger = alph.length-1 - alph.indexOf(letter.toLowerCase())
+let messageInput = document.querySelector('#message')
+let shiftValue = document.querySelector('#shift')
+let codedMessageInput = document.querySelector('#codeMessage')
+let codeBtn = document.querySelector('#codeBtn')
+let deleteBtn = document.querySelector('#deleteBtn')
 
-                if(alph.indexOf(letter.toLowerCase()) !== -1){
-                        letterFound = true;
-                }
-
-                if(!letterFound){
-                        encMsg += letter;
-                }
-
-                if(smallerOrBigger < shift && letterFound){
-                        if(letter === letter.toUpperCase()){
-                                encMsg += (alph[shift - (smallerOrBigger) - 1 ]).toUpperCase();
-                        }
-                        else{
-                                encMsg += alph[shift - (smallerOrBigger) - 1 ];
-                        }
-                }
-                else if(smallerOrBigger >= shift && letterFound){
-                        if(letter === letter.toUpperCase()){
-                                encMsg += (alph[ alph.indexOf(letter.toLowerCase()) + shift ]).toUpperCase();
-                        }
-                        else{
-                                encMsg += alph[ alph.indexOf(letter.toLowerCase()) + shift ];
-                        }
-                }
-        }
-        return encMsg;
-
-}
+codeBtn.addEventListener('click',()=>{
+    codedMessageInput.value = caesarCipher(messageInput.value,Number(shiftValue.value))
+    messageInput.value = ''
+})
+deleteBtn.addEventListener("click",()=>{
+    messageInput.value = ''
+    codedMessageInput.value = ''
+})
