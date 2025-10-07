882b386724c19ffa066d98a194a2b97ce74a854b
Loremispum <141552551+loremispum2k8@users.noreply.github.com>
2025-10-07 22:46:43 +0300

    Add basic html and css for the blocks and middle

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 import {caesarCipher} from "./functions.js"
 
-let messageInput = document.querySelector('#message')
-let shiftValue = document.querySelector('#shift')
-let codedMessageInput = document.querySelector('#codeMessage')
-let codeBtn = document.querySelector('#codeBtn')
-let deleteBtn = document.querySelector('#deleteBtn')
-let decode = document.querySelector('#decode')
+// let messageInput = document.querySelector('#message')
+// let shiftValue = document.querySelector('#shift')
+// let codedMessageInput = document.querySelector('#codeMessage')
+// let codeBtn = document.querySelector('#codeBtn')
+// let deleteBtn = document.querySelector('#deleteBtn')
+// let decode = document.querySelector('#decode')
 
-codeBtn.addEventListener('click',()=>{
-    shiftValue.value =Math.floor(Math.abs(shiftValue.value))
-    codedMessageInput.value = caesarCipher(messageInput.value,Number(shiftValue.value))
-    messageInput.value = ''
-})
-deleteBtn.addEventListener("click",()=>{
-    messageInput.value = ''
-    codedMessageInput.value = ''
-})
-decode.addEventListener('click',()=>{
-    if(shiftValue.value < 0){
-        shiftValue.value = Math.ceil(shiftValue.value)
-    }else{
-        shiftValue.value = -Math.floor(shiftValue.value)
-    }
-    messageInput.value= caesarCipher(codedMessageInput.value,Number(shiftValue.value))
-    codedMessageInput.value = ''
-})
+// codeBtn.addEventListener('click',()=>{
+//     shiftValue.value = Math.floor(Math.abs(shiftValue.value))
+//     codedMessageInput.value = caesarCipher(messageInput.value,Number(shiftValue.value))
+//     messageInput.value = ''
+// })
+// deleteBtn.addEventListener("click",()=>{
+//     messageInput.value = ''
+//     codedMessageInput.value = ''
+// })
+// decode.addEventListener('click',()=>{
+//     if(shiftValue.value < 0){
+//         shiftValue.value = Math.ceil(shiftValue.value)
+//     }else{
+//         shiftValue.value = -Math.floor(shiftValue.value)
+//     }
+//     messageInput.value= caesarCipher(codedMessageInput.value,Number(shiftValue.value))
+//     codedMessageInput.value = ''
+// })
+
