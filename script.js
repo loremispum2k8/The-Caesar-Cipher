e2355be9f8e21215ec144a46b809ed3666690811
Loremispum <141552551+loremispum2k8@users.noreply.github.com>
2025-10-08 02:02:25 +0300

    Add the coding function

--- script.js
+++ script.js
@@ -7,15 +7,7 @@ import {caesarCipher} from "./functions.js"
 // let deleteBtn = document.querySelector('#deleteBtn')
 // let decode = document.querySelector('#decode')
 
-// codeBtn.addEventListener('click',()=>{
-//     shiftValue.value = Math.floor(Math.abs(shiftValue.value))
-//     codedMessageInput.value = caesarCipher(messageInput.value,Number(shiftValue.value))
-//     messageInput.value = ''
-// })
-// deleteBtn.addEventListener("click",()=>{
-//     messageInput.value = ''
-//     codedMessageInput.value = ''
-// })
+
 // decode.addEventListener('click',()=>{
 //     if(shiftValue.value < 0){
 //         shiftValue.value = Math.ceil(shiftValue.value)
@@ -26,3 +18,33 @@ import {caesarCipher} from "./functions.js"
 //     codedMessageInput.value = ''
 // })
 
+
+
+// CODING MESSAGE 
+let codeShift = document.querySelector('#code-shift')
+let codeBtn = document.querySelector('.codify')
+let deleteCodeBtn = document.querySelector('.code-delete')
+let codeMessageInput = document.querySelector('#code-message-input')
+let codeMessageReciever = document.querySelector('#code-message-result')
+
+codeBtn.addEventListener('click',()=>{
+    codeShift.value = Math.floor(Math.abs(codeShift.value))
+    if(codeShift.value > 99){
+        codeShift.value = 99
+    }
+    codeMessageReciever.value = ''
+    codeMessageReciever.value = caesarCipher(codeMessageInput.value,Number(codeShift.value))
+})
+
+deleteCodeBtn.addEventListener('click',()=>{
+    codeMessageReciever.value = ''
+    codeMessageInput.value = ''
+})
+
+
+// DECODING MESSAGE
+let decodeShift = document.querySelector('#decode-shift')
+let decodeBtn = document.querySelector('.decodify')
+let deleteDecodeBtn = document.querySelector('.decode-delete')
+let decodeMessageInput = document.querySelector('#decode-message-input')
+let decodeMessageReciever = document.querySelector('#decode-message-result')
